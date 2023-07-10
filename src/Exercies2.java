7d1fe8976524e7cfa5a83888848dea463429eefe
silvia40 <silvia40@naver.com>
2023-07-10 16:46:43 +0900

    변수

---
+++ src/Exercies2.java
@@ -0,0 +1,13 @@
+
+public class Exercies2 {
+public static void main(String[] args) {
+	
+	
+	long regNo = 9610142152311L;
+	System.out.println(regNo);
+	
+	
+	System.out.println("¿");
+	
+}
+}
--- src/PrintExample.java
+++ src/PrintExample.java
@@ -1,7 +1,42 @@
 
 public class PrintExample {
-
+	
+	
+	
 	public static void main(String[] args) {
+		System.out.printf("ch = [%c]\n" , 'A');
+		System.out.printf("ch = [%10c]\n" , 'A');
+		System.out.printf("ch = [%-10c]\n" , 'A');
+		
+		System.out.printf("str = [%s]\n" , "Hello");
+		System.out.printf("str = [%10s]\n" , "Hello");
+		System.out.printf("str = [%-10s]\n" , "Hello");
+	}
+	
+	
+	public static void main5(String[] args) {
+		System.out.printf("real = [%f]\n" , 10.769);
+		System.out.printf("real = [%20f]\n" , 10.769);
+		System.out.printf("real = [%-20f]\n" , 10.769);
+		System.out.printf("real = [%020f]\n" , 10.769);
+		System.out.printf("real = [%+20f]\n" , 10.769);
+		System.out.printf("real = [%20.2]\n" , 10.769);
+	}
+	
+	public static void main4(String[] args) {
+		System.out.printf("num = [%d]\n", 99);
+		System.out.printf("num = [%10d]\n", 99);
+		System.out.printf("num = [%-10d]\n", 99);
+		System.out.printf("num = [%010d]\n", 99);
+		System.out.printf("num = [%+10d]\n", 99);
+	}
+
+	public static void main3(String[] args) {
+		System.out.print("hello\n");
+		System.out.println("hello");
+		
+	}
+	public static void main2(String[] args) {
 
 		int num1 = 10;
 		int num2 = 20;
